f629fa48539f42b3b0ec99791d7d5ffc3aab2325
Jwata <watanabe0621@gmail.com>
2018-03-19 00:47:16 +0900

    recover original task

--- goup_task.py
+++ goup_task.py
@@ -1,10 +1,10 @@
 import numpy as np
 from physics_sim import PhysicsSim
 
-class Task():
+class GoupTask():
     """Task (environment) that defines the goal and provides feedback to the agent."""
     def __init__(self, init_pose=None, init_velocities=None, 
-        init_angle_velocities=None, runtime=5., target_pos=None):
+        init_angle_velocities=None, runtime=5., target_z=None):
         """Initialize a Task object.
         Params
         ======
@@ -16,34 +16,36 @@ class Task():
         """
         # Simulation
         self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
-        self.action_repeat = 3
 
-        self.state_size = self.action_repeat * 6
+        self.state_size = len(self.get_state())
         self.action_low = 0
         self.action_high = 900
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
-
-    def get_reward(self):
-        """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
-        return reward
+        self.target_z = target_z
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
-        reward = 0
-        pose_all = []
-        for _ in range(self.action_repeat):
-            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
-            pose_all.append(self.sim.pose)
-        next_state = np.concatenate(pose_all)
+        done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+
+        current_z = self.sim.pose[2]
+        d = abs(current_z - self.target_z)
+        reward = -d
+
+        if current_z > self.target_z:
+            done = True
+            reward += 10.
+        elif done == True:
+            reward -= 10. # penalty for timeout and boundary over
+
+        next_state = self.get_state()
         return next_state, reward, done
 
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat) 
-        return state
+        return self.get_state()
+
+    def get_state(self):
+        return np.array(list(self.sim.pose) + list(self.sim.v) + list(self.sim.angular_v))
